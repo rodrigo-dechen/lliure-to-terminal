18b2c9a4fc6000117646aa4cc7893dfabaaa03f9
Rodrigo <mestri.rodrigo@gmail.com>
2020-10-25 13:27:09 -0300

    ## Implementação
    - copy agora consegue copiar arquivos individuas ou usar o curinga (*) para copiar varios arquivos que começem con determinhado nome

--- copy/copy.php
+++ copy/copy.php
@@ -1,78 +1,178 @@
 <?php
 
 class copy extends terminal{
-    public function rum(){
+    private $log = false;
 
-        if($log = (($k = array_search('-l', $this->args)) !== false)) unset($this->args[$k]);
-        if($repalce = (($k = array_search('-r', $this->args)) !== false)) unset($this->args[$k]);
-
-
-        if(!isset($this->args[0])){
-            do{
-                $this->args[0] = $this->gets('Digite a pasta origem > ');
-
-                $f = $teste = parse_url($this->args[0]);
-                if(!isset($teste['scheme'])) $f = $this->path . DIRECTORY_SEPARATOR . $this->args[0];
-
-            }while(!file_exists($f));
-        }
-
-
-        if(!isset($this->args[1])) $this->args[1] = $this->gets('Digite a pasta destino > ');
-
-        $oldname = $oldpath = $this->args[0];
-        $newname = $newpath = $this->args[1];
-
-        $teste = parse_url($oldname);
-        if(!isset($teste['scheme'])) $oldpath = $this->path . DIRECTORY_SEPARATOR . $oldname;
-
-        $teste = parse_url($newname);
-        if(!isset($teste['scheme'])) $newpath = $this->path . DIRECTORY_SEPARATOR . $newname;
-
-        self::copyPath($oldpath, $newpath, $log, $repalce);
+    public function help(){
+        self::printr(implode(PHP_EOL, [
+            'Copia arquivo(s) e pastas podendo modificar altomaticamente seu conteudo',
+            'O original pode ter o caracter "*" (coringa) no final, com isso ira copiar todos os arquivos que comecem com o original',
+            '',
+            "\t-l\tmostra um log interno do script",
+            "\t-r\ttroca as ocorencias no conteudo do nome original no destino ",
+            "\t-ed\tdesconsidera os diretorios",
+            "\t-ef\tdesconsidera os arquivos",
+        ]));
     }
 
-    private static function copyPath($oldpath, $newpath, $log = false, $repalce = false){
-        if(!is_dir($oldpath)){
-            self::printr('Diretorio origem não encontado: ' . $oldpath);
-            return;
+    public function rum(){
+        if(array_search('-h', $this->args) !== false) return $this->help();
+
+        if($this->log = (($k = array_search('-l', $this->args)) !== false)) unset($this->args[$k]);
+        if($repalce = (($k = array_search('-r', $this->args)) !== false)) unset($this->args[$k]);
+        if($excludeDirectory = (($k = array_search('-ed', $this->args)) !== false)) unset($this->args[$k]);
+        if($excludeFile = (($k = array_search('-ef', $this->args)) !== false)) unset($this->args[$k]);
+
+        while(!isset($this->args[0]) || !self::sourceExists($this->absolutePath($this->args[0]))){
+            self::printr('Original não encontado' . ((isset($this->args[0]))? ': '. $this->absolutePath($this->args[0]): '') . PHP_EOL);
+            $this->args[0] = $this->gets('Digite o original > ');
         }
 
-        if (!is_dir($newpath)) mkdir($newpath, 0755);
+        if(!isset($this->args[1]))
+            $this->args[1] = $this->gets('Digite a copia > ');
 
-        $oldname = basename($oldpath);
-        $newname = basename($newpath);
+        while(self::copyExists($this->absolutePath($this->args[0]), $this->absolutePath($this->args[1]))){
+            if($this->gets('Copia existe. Sobresquevela (S ou N) > ') === 'S') break;
+            $this->args[1] = $this->gets('Digite a copia > ');
+        }
 
-        foreach(scandir($oldpath) as $f){
-            if ($f == '.' || $f == '..') continue;
+        self::copyPath($this->absolutePath($this->args[0]), $this->absolutePath($this->args[1]), $repalce, $excludeFile, $excludeDirectory);
+    }
 
-            if (is_dir($oldpath . DIRECTORY_SEPARATOR . $f)){
-                if($log) self::printr("Copiando diretorio > $f\n");
-                self::copyPath($oldpath . DIRECTORY_SEPARATOR . $f, $newpath . DIRECTORY_SEPARATOR . $f);
+    private function copyPath($source, $destiny, $repalce = false, $excludeFile = false, $excludeDirectory = false){
+        $generic = self::generic($source, $source);
+        $oPath = dirname($source);
+        $oName = basename($source);
+        $nPath = dirname($destiny);
+        $nName = basename($destiny);
 
-            }else{
-                $target = '';
-                if(preg_match('/^' . preg_quote($oldname) . '/', $f)){
-                    $ext = substr($f, strlen($oldname));
-                    if(!file_exists($newpath . DIRECTORY_SEPARATOR . $f)
-                    &&(!file_exists($newpath . DIRECTORY_SEPARATOR . $newname . $ext))){
-                        if($log) self::printr("Copiando arquivo > $f\n");
-                        copy(
-                            ($oldpath . DIRECTORY_SEPARATOR . $f),
-                            ($target = $newpath . DIRECTORY_SEPARATOR . $newname . $ext)
-                        );
-                    }
-                }else{
-                    if($log) self::printr("Copiando arquivo > $f\n");
-                    copy(
-                        ($oldpath . DIRECTORY_SEPARATOR . $f),
-                        ($target = $newpath . DIRECTORY_SEPARATOR . $f)
-                    );
+        $oFiles = [];
+        if($generic){
+            $regQry = '/^' . preg_quote($oName) . '/';
+            foreach(scandir($oPath) as $fileName){
+                if(!($fileName === '.' || $fileName === '..') && preg_match($regQry, $fileName)){
+                    $oFiles[] = $oPath . DIRECTORY_SEPARATOR . $fileName;
                 }
+            }
+        }else{
+            $oFiles[] = $source;
+        }
 
-                if($repalce && $target) file_put_contents($target, implode($newname, explode($oldname, file_get_contents($target))));
+        foreach($oFiles as $k => $file){
+            if(($excludeDirectory && is_dir($file)) || ($excludeFile && is_file($file))){
+                unset($oFiles[$k]);
+            }
+        }
+
+        foreach($oFiles as $k => $file){
+            $copy = (($generic)? $nPath . DIRECTORY_SEPARATOR . $nName . substr(basename($file), strlen($oName)): $destiny);
+
+            if(!file_exists($copy)){
+                if(is_file($file)){
+                    if($this->log) self::printr("Copiando arquivo:: {$file} >> {$copy}" . PHP_EOL);
+                    copy($file, $copy);
+
+                    if($repalce && !self::isBinary($copy)){
+                        if($this->log) self::printr("Subistituindio:: [{$oName}] >> [{$nName}] :: {$copy}" . PHP_EOL);
+                        file_put_contents($copy, implode($nName, explode($oName, file_get_contents($copy))));
+                    }
+
+                }elseif(is_dir($file)){
+                    if($this->log) self::printr("Copiando diretorio:: {$file} >> {$copy}" . PHP_EOL);
+                    $this->copyDiretory($file, $copy, $repalce);
+
+                }
             }
         }
     }
 
+    private function copyDiretory($source, $destiny, $repalce = false){
+        if (!is_dir($destiny)) mkdir($destiny, 0755);
+
+        $oldname = basename($source);
+        $newname = basename($destiny);
+
+        foreach(scandir($source) as $f){
+            if ($f == '.' || $f == '..') continue;
+
+            $target = false;
+            $file = ($source . DIRECTORY_SEPARATOR . $f);
+
+            if (is_dir($file)){
+                $copy = ($destiny . DIRECTORY_SEPARATOR . $f);
+                if($this->log) self::printr("Copiando diretorio:: {$file} >> {$copy}" . PHP_EOL);
+                $this->copyDiretory($file, $copy);
+
+            }else{
+                if(preg_match('/^' . preg_quote($oldname) . '/', $f))
+                    $copy = ($target = $destiny . DIRECTORY_SEPARATOR . $newname . substr($f, strlen($oldname)));
+                else
+                    $copy = ($destiny . DIRECTORY_SEPARATOR . $f);
+
+                if(!file_exists($copy)){
+                    if($this->log) self::printr("Copiando arquivo:: {$file} >> {$copy}" . PHP_EOL);
+                    copy($file, $copy);
+                }
+
+                if($repalce && $target && !self::isBinary($target)) {
+                    if($this->log) self::printr("Subistituindio:: [{$oldname}] >> [{$newname}] :: {$target}" . PHP_EOL);
+                    file_put_contents($target, implode($newname, explode($oldname, file_get_contents($target))));
+                }
+            }
+        }
+    }
+
+    private function absolutePath ($arg){
+        $teste = parse_url($arg);
+        if(!isset($teste['scheme']))
+            return $this->path . DIRECTORY_SEPARATOR . $arg;
+        return $arg;
+    }
+
+    private static function sourceExists($old){
+        $generic = self::generic($old, $old);
+
+        $path = dirname($old);
+        $name = basename($old);
+
+        if(!$generic && file_exists($old)) return true;
+
+        $regQry = '/^' . preg_quote($name) . '/';
+        foreach(scandir($path) as $f){
+            if($f == '.' || $f == '..') continue;
+            if(preg_match($regQry, $f)) return true;
+        } return false;
+    }
+
+    private static function generic($path, &$clear = ''){
+        if (substr($path, -1) !== '*') return false;
+        $clear = substr($clear, 0, -1);
+        return true;
+    }
+
+    private static function copyExists($old, $new){
+        $generic = self::generic($old, $old);
+
+        if(!$generic && file_exists($new)) return true;
+
+        $oPath = dirname($old);
+        $oName = basename($old);
+        $nPath = dirname($new);
+        $nName = basename($new);
+        $nRefs = [];
+
+        $regQry = '/^' . preg_quote($oName) . '(.*)/';
+        foreach(scandir($oPath) as $fileName) if(!($fileName === '.' || $fileName === '..') && preg_match($regQry, $fileName, $matches) && isset($matches[1]))
+            $nRefs[] = preg_quote($nName . $matches[1]);
+
+        $regQry = '/^' . implode('$|^', $nRefs) . '$/';
+        foreach(scandir($nPath) as $fileName) if(!($fileName === '.' || $fileName === '..') && preg_match($regQry, $fileName))
+            return true;
+
+        return false;
+    }
+
+    private static function isBinary($filename){
+        return (substr(finfo_file(finfo_open(FILEINFO_MIME), $filename), 0, 4) === 'text');
+    }
 }
