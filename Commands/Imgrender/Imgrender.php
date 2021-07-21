aa3176e51fc745537859eff3a709522f1661ccb7
Rodrigo Dechen <mestri.rodrigo@gmail.com>
2021-07-21 14:58:23 -0300

    Excluindo as imgs ja convertidas em webp da lista

--- Commands/Imgrender/Imgrender.php
+++ Commands/Imgrender/Imgrender.php
@@ -23,7 +23,7 @@ class Imgrender extends Terminal
 		foreach(scandir($this->path) as $filename){
 			$file = $this->path . DIRECTORY_SEPARATOR . $filename;
 			$mimetype = mime_content_type($file);
-			if(!(substr($mimetype, 0, 6) == 'image/')){
+			if($mimetype == 'image/webp' || !(substr($mimetype, 0, 6) == 'image/')){
 				continue;
 			}
 			$files[] = [
